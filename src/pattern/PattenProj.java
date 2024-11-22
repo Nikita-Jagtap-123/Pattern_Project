2c5130aba38df6e56fd0287d95b9cf288f35f913
Nikita-Jagtap-123 <nikitajagtap8442@gmail.com>
2024-11-22 10:55:39 +0530

    first commit

--- src/pattern/PattenProj.java
+++ src/pattern/PattenProj.java
@@ -3,9 +3,9 @@ package pattern;
 public class PattenProj {
 	public static void main(String[] args) {
 		int i,j;
-		for(i=1;i<=3;i++)//outer for loop -> no. of rows
+		for(i=1;i<=5;i++)//outer for loop -> no. of rows
 		{
-			for(j=1;j<=3;j++)//Inner for loop -> no. of columns
+			for(j=1;j<=5;j++)//Inner for loop -> no. of columns
 			{
 				System.out.print(" * ");
 			}
